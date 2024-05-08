c5af3c9c359b50510749c0841a96030ff56494ce
ruttlea <ruttlea@shaw.ca>
2024-05-07 23:28:58 -0600

    assignment 3: emit events, improve error messages

--- src/SanctionCoin.sol
+++ src/SanctionCoin.sol
@@ -10,40 +10,39 @@ contract SanctionCoin is ERC20 {
     address private admin;
     mapping(address => bool) banned;
 
+    event AccountBanned(address account);
+    event AccountUnbanned(address account);
+
     constructor() ERC20("SC", "SanctionCoin") {
         admin = msg.sender;
     }
 
-    function ban(address addr) public {
-        require(msg.sender == admin, "only admin can ban");
-        require(addr != admin, "admin cannot be banned");
-        require(banned[addr] == false, "user already banned");
-
-        banned[addr] = true;
-        // TODO: emit event
-    }
-
-    function unban(address addr) public {
-        require(msg.sender == admin, "only admin can ban");
-        require(addr != admin, "admin cannot be banned");
-        require(banned[addr] == true, "user is not banned");
-
-        banned[addr] = false;
-        // TODO: emit event
-    }
-
     function mint(address to, uint256 amount) public {
-        require(msg.sender == admin, "only admin can mint");
-        require(banned[to] == false, "receiver is banned");
+        require(msg.sender == admin, "Only admin can mint");
+        require(banned[to] == false, "Receiver is banned");
         _mint(to, amount);
-        // TODO: emit event
     }
 
     function give(address to, uint256 amount) public {
-        require(banned[msg.sender] == false, "sender is banned");
-        require(banned[to] == false, "receiver is banned");
+        require(banned[msg.sender] == false, "Sender account is banned");
+        require(banned[to] == false, "Receiver account is banned");
         require(balanceOf(msg.sender) >= amount);
         transfer(to, amount);
-        // TODO: emit event
+    }
+
+    function ban(address account) public {
+        require(msg.sender == admin, "Only admin can ban accounts");
+        require(account != admin, "Admin cannot be banned");
+        require(banned[account] == false, "Account already banned");
+        banned[account] = true;
+        emit AccountBanned(account);
+    }
+
+    function unban(address account) public {
+        require(msg.sender == admin, "Only admin can ban accounts");
+        require(account != admin, "Admin cannot be banned");
+        require(banned[account] == true, "Account is not banned");
+        banned[account] = false;
+        emit AccountUnbanned(account);
     }
 }
